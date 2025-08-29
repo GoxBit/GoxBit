c594a6b45f3ec0fe5774030092ddd0539f4d6d5e
Luis Duran <61667213+GoxBit@users.noreply.github.com>
2025-08-29 01:10:00 -0600

    fix models higlights, carousel and all models

--- GoxBit/Pages/Models.razor.cs
+++ GoxBit/Pages/Models.razor.cs
@@ -9,7 +9,8 @@ namespace GoxBit.Pages
     public partial class Models : ComponentBase
     {
         private List<Model> Showcase = new List<Model>();
-
+        private bool showAllModels = false;
+        private void ShowModels() => showAllModels = true;
         protected override async Task OnInitializedAsync()
         {
             var jsonFile = File.ReadAllText("wwwroot/Models.json");
