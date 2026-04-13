b3f1e65acbeb3f65c086b0f402860fd48abe537a
Luis Duran <61667213+GoxBit@users.noreply.github.com>
2026-04-12 19:07:17 -0600

    use sketchfab api

--- GoxBit/Pages/Models.razor.cs
+++ GoxBit/Pages/Models.razor.cs
@@ -1,22 +1,54 @@
 ﻿using Microsoft.AspNetCore.Components;
-using System.Text.Json;
-using System;
-using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json.Serialization;
 using GoxBit.Components;
 
 namespace GoxBit.Pages
 {
     public partial class Models : ComponentBase
     {
+        private const string SketchfabModelsUrl = "https://api.sketchfab.com/v3/models?user=goxbit&archives_flavours=false";
+
+        [Inject]
+        private HttpClient Http { get; set; } = default!;
+
         private List<Model> Showcase = new List<Model>();
+        private int carouselSelectedIndex = 0;
+        private int carouselRenderKey = 0;
         private bool showAllModels = false;
         private void ShowModels() => showAllModels = true;
+
         protected override async Task OnInitializedAsync()
         {
-            var jsonFile = File.ReadAllText("wwwroot/Models.json");
-            Showcase = JsonSerializer.Deserialize<List<Model>>(jsonFile);
+            var response = await Http.GetFromJsonAsync<SketchfabModelsResponse>(SketchfabModelsUrl);
+
+            Showcase = response?.Results?
+                .Where(model => !string.IsNullOrWhiteSpace(model.Name) && !string.IsNullOrWhiteSpace(model.EmbedUrl))
+                .Select(model => new Model
+                {
+                    Name = model.Name!,
+                    Url = model.EmbedUrl!,
+                    Tools = new List<ToolType>()
+                })
+                .ToList() ?? new List<Model>();
+
+            carouselSelectedIndex = 0;
+            carouselRenderKey++;
+        }
+
+        private sealed class SketchfabModelsResponse
+        {
+            [JsonPropertyName("results")]
+            public List<SketchfabModel>? Results { get; set; }
+        }
+
+        private sealed class SketchfabModel
+        {
+            [JsonPropertyName("name")]
+            public string? Name { get; set; }
+
+            [JsonPropertyName("embedUrl")]
+            public string? EmbedUrl { get; set; }
         }
     }
-
-    
 }
